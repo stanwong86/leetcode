3aaabe5cc95c0a0d8a2c47a2958e36609eac1dd7
Stanley Wong <stanwong86@gmail.com>
2025-07-09 23:18:22 -0400

    8_StringToInteger

--- string/8_StringToInteger.py
+++ string/8_StringToInteger.py
@@ -1,20 +1,19 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        # 1. Whitespace
+        
         s = s.strip()
 
         if not s:
             return 0
 
-        # 2. Signed
         i = 0
         sign = 1
         if s[i] == '-':
             sign = -1
             i += 1
-        elif s[0] == '+':
+        elif s[i] == '+':
             i += 1
-
+        
         num = 0
         while i < len(s) and s[i].isdigit():
             num = num * 10 + int(s[i])
@@ -29,3 +28,4 @@ class Solution:
         elif num > INT_MAX:
             return INT_MAX
         return num
+        
