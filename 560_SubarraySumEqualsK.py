669a8eb850cdf19db11587c968db5766ec510dcf
Stanley Wong <stanwong86@gmail.com>
2025-07-16 20:59:33 -0400

    Subarray Sum Equals K

---
+++ 560_SubarraySumEqualsK.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        totals = {0: 1}
+
+        total = count = 0
+
+        for num in nums:
+            total += num
+
+            if total-k in totals:
+                count += totals[total-k]
+            
+            if total not in totals:
+                totals[total] = 0
+            totals[total] += 1
+        
+        return count
