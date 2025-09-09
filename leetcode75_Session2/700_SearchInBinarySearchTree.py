a8e47dfb0eee5f489b69c956f2be476d4758e13e
Stanley Wong <stanwong86@gmail.com>
2025-09-08 20:47:49 -0400

    Add/update leetcode75_Session2/700_SearchInBinarySearchTree.py

---
+++ leetcode75_Session2/700_SearchInBinarySearchTree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return
+        
+        def dfs(node):
+            if not node or node.val == val:
+                return node
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            return left or right        
+
+        return dfs(root)
